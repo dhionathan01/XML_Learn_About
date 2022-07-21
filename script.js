47694a6aa5b8c954ba55bceeb57d7a4ae863237a
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2022-07-21 14:58:43 -0300

    Fazendo a inserção dos dados no front com js

--- script.js
+++ script.js
@@ -5,14 +5,51 @@ function getFilmes() {
     xmlhttp.onreadystatechange = () => {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             let XMLFilmes = xmlhttp.responseText
-            console.log(XMLFilmes)
+            //console.log(XMLFilmes)
 
             let parser = new DOMParser() // Criando objeto para converter string em dom
             domFilmes = parser.parseFromString(XMLFilmes, 'text/xml') // convertendo xml em uma árvore de elementos
-            console.log(domFilmes)
+            //console.log(domFilmes)
 
             jsonFilmes = xmlToJson(domFilmes)
             console.log(jsonFilmes)
+
+            for (let i in jsonFilmes['filmes']['filme']) {
+                console.log(jsonFilmes['filmes']['filme'][i])
+
+                let divRow = document.createElement('div')
+                divRow.className = 'row'
+
+                let divCol = document.createElement('div')
+                divCol.className = 'col'
+
+                let p1 = document.createElement('p')
+                p1.innerHTML = '<strong>Título:</strong> Titulo do filme'
+
+                let p2 = document.createElement('p')
+                p2.innerHTML = '<strong>Resumo:</strong> Resumo do filme'
+
+                let p3 = document.createElement('p')
+                p3.innerHTML = '<strong>Gênero:</strong> Gênero do filme'
+
+                let p4 = document.createElement('p')
+                p4.innerHTML = '<strong>Elenco:</strong> Elenco do filme'
+
+                let p5 = document.createElement('p')
+                p5.innerHTML = '<strong>Data de lançamento:</strong> Data lançamento do filme'
+
+                let hr = document.createElement('hr')
+
+                divRow.appendChild(divCol)
+                divCol.appendChild(p1)
+                divCol.appendChild(p2)
+                divCol.appendChild(p3)
+                divCol.appendChild(p4)
+                divCol.appendChild(p5)
+                divCol.appendChild(hr)
+
+                document.getElementById('lista').appendChild(divRow)
+            }
             
         }
         if (xmlhttp.readyState == 4 && xmlhttp.status == 404) {
